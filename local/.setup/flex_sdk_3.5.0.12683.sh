08264d3c6946828f6494bea62e74c1e8bdbfbe82
clairvy <clairvy@gmail.com>
2010-02-03 09:35:27 +0900

    append setting for flex sdk.

---
+++ local/.setup/flex_sdk_3.5.0.12683.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+root=../modules/flex_sdk_3.5.0.12683
+( cd bin && ls -d ../$root/bin/* | egrep -v '\.(config|exe|bat)$' | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
+( cd lib && ls -d ../$root/lib/* | egrep '\.jar$' | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
