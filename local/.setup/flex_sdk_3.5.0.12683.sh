e52fcd2f7f52e7f4bff00e981b21d9fad403a960
clairvy <clairvy@gmail.com>
2010-02-03 14:15:31 +0900

    append runtime dir setting for flex air.

--- local/.setup/flex_sdk_3.5.0.12683.sh
+++ local/.setup/flex_sdk_3.5.0.12683.sh
@@ -5,3 +5,4 @@ root=../modules/flex_sdk_3.5.0.12683
 ( cd bin && ls -d ../$root/bin/* | egrep -v '\.(config|exe|bat)$' | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
 ( cd lib && ls -d ../$root/lib/* | egrep '\.jar$' | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
 ( ls -d $root/frameworks | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
+( ls -d $root/runtimes | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
