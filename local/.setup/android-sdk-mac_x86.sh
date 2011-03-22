95b558c7e5b32f9f3e06d9a1f9821ba30e5a80d8
clairvy <clairvy@gmail.com>
2011-03-22 16:24:35 +0900

    SETUP rename 86 to x86

--- local/.setup/android-sdk-mac_x86.sh
+++ local/.setup/android-sdk-mac_x86.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-root=../modules/android-sdk-mac_86
+root=../modules/android-sdk-mac_x86
 ( cd bin && ls -d ../$root/tools/* | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
 rm android; ln -s $root android
