14ed5e7a3a8a79bda3450f6119abbc6a611137b3
clairvy <clairvy@gmail.com>
2011-03-23 17:58:40 +0900

    SETUP append platform-tools for android-sdk

--- local/.setup/android-sdk-mac_x86.sh
+++ local/.setup/android-sdk-mac_x86.sh
@@ -2,4 +2,5 @@
 
 root=../modules/android-sdk-mac_x86
 ( cd bin && ls -d ../$root/tools/* | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
+( cd bin && ls -d ../$root/platform-tools/* | xargs -I {} echo 'rm `basename {}`; ln -s {} `basename {}`' | sh )
 rm android; ln -s $root android
