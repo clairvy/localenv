6862c67822c84ad978958129c7cc6c5f9aed7058
clairvy <clairvy@gmail.com>
2012-07-11 12:45:31 +0900

    append initialize script

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+for f in .*; do
+  if [ x"$f" == x'.' -o x"$f" == x'..' ]; then
+    continue
+  fi
+  if [ x"$f" == x'.gitmodules' -o x"$f" == x'.git' -o x"$f" == x'.gitignore' ]; then
+    continue
+  fi
+  if [ x"$f" == x'.hosts' ]; then
+    continue
+  fi
+  pushd ~
+#pwd
+  if [ -f $f ]; then
+#    echo mv $f $f.org
+    mv $f $f.org
+  fi
+#  echo ln -s s/localenv/$f .
+  ln -s s/localenv/$f .
+  popd
+#pwd
+done
